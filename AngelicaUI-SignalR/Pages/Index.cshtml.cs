5f6a1a877ae3257addf6f5a4226ed6185759c669
sickoovit <omarnasser665@gmail.com>
2025-02-21 03:16:13 +0200

    Add project files.

---
+++ AngelicaUI-SignalR/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AngelicaUI_SignalR.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
